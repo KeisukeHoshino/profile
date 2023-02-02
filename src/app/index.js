756ac3179f018e25ff8bd1cbabf06c8001581b8b
星野　圭助 <po258096@gmail.com>
2023-02-02 23:31:43 +0900

    体裁終生

--- src/app/index.js
+++ src/app/index.js
@@ -20,3 +20,12 @@ $(function () {
         return false;
     });
 });
+// firstViewのアクション
+$(function () {
+    setTimeout(function () {
+        $('.start p').fadeIn(1600);
+    }, 500); //0.5秒後にロゴをフェードイン!
+    setTimeout(function () {
+        $('.start').fadeOut(500);
+    }, 2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
