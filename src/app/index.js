a74459bd418ba4a4669838a6a47a534d4ef20ed6
Keisuke-Hoshino <po258096@gmail.com>
2023-02-04 14:45:42 +0900

    スキルと出身地のレスポンシブ対応

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,4 @@
+//スライdー
 $(".slide-items").slick({
     centerMode: true, // 前後スライドを部分表示
     centerPadding: "10%", // 両端の見切れるスライド幅
@@ -15,17 +16,34 @@ $(function () {
         var speed = 500;
         var href = $(this).attr("href");
         var target = $(href == "#" || href == "" ? "html" : href);
-        var position = target.offset().top - headerHeight -10;
+        var position = target.offset().top - headerHeight - 10;
         $("html, body").animate({ scrollTop: position }, speed, "swing");
         return false;
     });
 });
+
 // firstViewのアクション
+// $(function () {
+//     setTimeout(function () {
+//         $('.start p').fadeIn(1600);
+//     }, 500); //0.5秒後にロゴをフェードイン!
+//     setTimeout(function () {
+//         $('.start').fadeOut(500);
+//     }, 2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+// });
+
+// フェードイン
+
 $(function () {
-    setTimeout(function () {
-        $('.start p').fadeIn(1600);
-    }, 500); //0.5秒後にロゴをフェードイン!
-    setTimeout(function () {
-        $('.start').fadeOut(500);
-    }, 2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+    $(window).scroll(function () {
+        $("#skill").each(function () {
+            let offset = $("#skill").offset().top;
+            let scroll = $(window).scrollTop();
+
+            if (scroll > offset - 500) {
+                $("#skill").addClass("active");
+                $(".action").addClass("skillPercent");
+            }
+        });
+    });
 });
