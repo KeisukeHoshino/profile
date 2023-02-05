b96078daa4711dfd4d35ef80c0f98d152209b91d
Keisuke-Hoshino <po258096@gmail.com>
2023-02-06 08:10:39 +0900

    好きなアニメと出身地のレスポンシブ対応

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,6 @@
-//スライdー
+/**
+ * スライダー
+ */
 $(".slide-items").slick({
     centerMode: true, // 前後スライドを部分表示
     centerPadding: "10%", // 両端の見切れるスライド幅
@@ -9,8 +11,10 @@ $(".slide-items").slick({
     nextArrow: '<button class="slide-arrow next-arrow"></button>',
 });
 
+/**
+ * スムーススクロール
+ */
 $(function () {
-    //スムーススクロール
     const headerHeight = $("header").outerHeight();
     $('a[href^="#"]').click(function () {
         var speed = 500;
@@ -22,7 +26,10 @@ $(function () {
     });
 });
 
-// firstViewのアクション
+/**
+ * firstViewのアクション
+ */
+//
 // $(function () {
 //     setTimeout(function () {
 //         $('.start p').fadeIn(1600);
@@ -32,18 +39,68 @@ $(function () {
 //     }, 2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 // });
 
-// フェードイン
-
+/**
+ * スキルのフェードイン
+ */
 $(function () {
     $(window).scroll(function () {
         $("#skill").each(function () {
             let offset = $("#skill").offset().top;
             let scroll = $(window).scrollTop();
 
-            if (scroll > offset - 500) {
+            if (scroll > offset - 700) {
                 $("#skill").addClass("active");
                 $(".action").addClass("skillPercent");
             }
         });
     });
 });
+
+/**
+ * 好きなアニメの処理
+ * ジャンルを選択したら背景を変更
+ * 画像を変更する
+ */
+window.addEventListener("load", () => {
+    let anime_action = document.querySelectorAll(".anime_action");
+
+    let genre = document.querySelectorAll(".genre");
+    genre[0].style.backgroundColor = "rgb(14 165 233 / var(--tw-bg-opacity))";
+    for (let i = 0; i < genre.length; i++) {
+        genre[i].addEventListener("click", function () {
+            for (let i = 0; i < genre.length; i++) {
+                genre[i].style.backgroundColor =
+                    "rgb(186 230 253 / var(--tw-bg-opacity))";
+                anime_action[i].style.display = "none";
+            }
+            this.style.backgroundColor =
+                "rgb(14 165 233 / var(--tw-bg-opacity))";
+            anime_action[i].style.display = "flex";
+        });
+    }
+    // ((x) => {
+    //     x.addEventListener("click", (e) => {
+    //         x.style.backgroundColor = "rgb(14 165 233 / var(--tw-bg-opacity))";
+    //         genre_action[0].style.display = "none";
+    //         if(e ==0) {
+    //             console.log(a);
+    //         }
+    //         console.log(e);
+    //     });
+    // });
+});
+
+/**
+ * 出身地のアコーディオン
+ */
+const menu = document.querySelectorAll(".js-menu");
+
+function toggle() {
+    const content = this.nextElementSibling;
+    this.classList.toggle("is-active");
+    content.classList.toggle("is-open");
+}
+
+for (let i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("click", toggle);
+}
